184065b5b952352cbf5c1898284d1aca37e0858e
kobiowuquadri <kobiowuq@gmail.com>
2023-10-23 11:16:39 -0500

    update project folder

--- server/helpers/generateAdminsToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateAdminsToken = (id, email, role) => {
-  return jwt.sign({ id, email, role }, process.env.JWT_SECRET);
-};
-
-module.exports = {
-  generateAdminsToken,
-};
--- server/helpers/generateUsersToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateUsersToken = (id, email) => {
-  return jwt.sign({ id, email }, process.env.JWT_SECRET);
-};
-
-module.exports = {
-  generateUsersToken,
-};

77e55b751783d4c9d0e376702b2ed68409a6858c
kobiowuquadri <kobiowuq@gmail.com>
2023-11-05 06:57:15 -0600

    add users to admin dashboard

--- client/src/Pages/Dashboards/AdminDashboard/AdminDashboard.jsx
+++ client/src/Pages/Dashboards/AdminDashboard/AdminDashboard.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Slidebar from '../Slidebar/Sliderbar'
+import axios from 'axios'
+import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 function AdminDashboard() {
+  const [allUsers,setallUsers ] = useState([])
+
+  const displayUsers = async () => {
+    try{
+
+      const response = await axios.get('http://localhost:5000/allusers')
+      console.log(response?.data?.users)
+      setallUsers(response?.data?.users)
+    }
+    catch(err){
+      console.log(err.message)
+    }
+  }
+  useEffect(() => {
+    displayUsers()
+  }, [])
   return (
-    <div>
+    <div className='board'>
        <Slidebar/>
-       <h1>WELCOME TO ADMIN DASHBOARD</h1>
+       <div className='main__board'>
+       <h1 className='text-white'>WELCOME TO ADMIN DASHBOARD</h1>
+          <MDBTable align='middle'>
+          <MDBTableHead className='text-white'>
+            <tr>
+              <th scope='col text-white'>Name</th>
+              <th scope='col text-white'>Role</th>
+              <th scope='col text-white'>Position</th>
+              <th scope='col text-white'>Actions</th>
+            </tr>
+          </MDBTableHead>
+    
+          <MDBTableBody>
+            
+           {allUsers.map((user, index) => {
+            return(
+              <tr>
+                <td>
+                <div className='d-flex align-items-center'>
+                  <img
+                    src='https://mdbootstrap.com/img/new/avatars/8.jpg'
+                    alt=''
+                    style={{ width: '45px', height: '45px' }}
+                    className='rounded-circle'
+                  />
+                  <div className='ms-3'>
+                    <p className='fw-bold mb-1 text-white'>{user.username}</p>
+                    <p className='mb-0 text-white'>{user.email}</p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p className='fw-normal mb-1 text-white'>{user.role}</p>
+              </td>
+              <td>
+                <MDBBadge color='success' pill>
+                  Active
+                </MDBBadge>
+              </td>
+              <td>
+                <MDBBtn color='link' rounded size='sm'>
+                  Edit
+                </MDBBtn>
+              </td>
+              </tr>
+            )
+           })}
+           
+          
+          </MDBTableBody>
+        </MDBTable>
+       </div>
     </div>
   )
 }

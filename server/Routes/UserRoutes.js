fe085f987a6564f8c74bc08cfcaaff43654c457f
kobiowuquadri <kobiowuq@gmail.com>
2023-11-30 07:16:24 -0600

    add more user routes

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -3,7 +3,7 @@ const { signInUser, creditAnotherUser } = require('../Controllers/userController
 const userRouter = express.Router()
 
 // // Signin
-userRouter.post('/signin', signInUser)
+userRouter.post('/signin-customer', signInUser)
 
 // credit other user
 userRouter.put('/tranfer-funds', creditAnotherUser)
